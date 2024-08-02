9aa6a1341114488361f9934015fdca56c729c177
inbok <inbok.corp@gmail.com>
2024-08-02 13:34:09 +0900

    first commit

---
+++ src/main/java/com/example/helloapi/controller/HelloController.java
@@ -0,0 +1,22 @@
+package com.example.helloapi.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+public class HelloController {
+
+    @RequestMapping("/hello")
+    public Object GetHello() {
+
+        Map<String, String> map = new HashMap<>();
+
+        map.put("rtn","hello");
+
+        return map;
+
+    }
+}
